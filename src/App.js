4791b421cafa603885a8c675dcedc2dec71b184e
RafaRomero8 <RafaRomerox@gmail.com>
2022-02-05 20:31:47 -0600

    modify sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import man from './assets/Saly-19.png'
 
 function App() {
   return (
@@ -9,13 +10,29 @@ function App() {
       </header>
 
     <main className='main'>
-      
+    
       <section className='section_first'>
-       <h3>How the app works</h3>
-       <div className='section_first-gadget'>
+      <h3>How the app works</h3>
+        <section className='section_first-container'>
+        <div className='section_first-back'>
 
-       </div>
+          <div className='section_first-gadget'>
+           <h3>Find your Gadget</h3>
+            <img className='section_first-image' src={man}></img>
+             <button className='section_first-button'>Get started</button>
+          </div>
 
+        </div>
+            <div className='section_first-description'>
+              <h4>Create an account</h4>
+                <h2>Discover original products</h2>
+            <p>There are more than 950 categories updated daily based on 
+              trending websites reviews an users rating.</p>
+
+           </div>
+        </section>
+     
+  
       </section>
 
       <section className='section_second'>
