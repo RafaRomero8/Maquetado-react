3e92ffb6c2230f8ada8499e2fcdc4a0b7938303f
RafaRomero8 <RafaRomerox@gmail.com>
2022-02-12 00:18:24 -0600

    desktop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import "./query.css"
 import firstMobile from "./assets/firstMobile.png";
 import secondMobile from "./assets/second.png";
 import therdMobile from "./assets/therd.png";
@@ -40,19 +41,23 @@ function App() {
         <section className="section_first">
           <h3>How the app works</h3>
           <div className="section_first-description">
+           
+           
             <h4>Create an account</h4>
-
             <h2>Discover original products</h2>
             <p>
               There are more than 950 categories updated daily based on trending
               websites reviews an users rating.
             </p>
-
+            <div className='circle_desktop'> </div>
+            
+           
             <img
               className="section_first-image"
               src={firstMobile}
               alt="first phone"
             ></img>
+            
           </div>
         </section>
         <section className="section_second">
@@ -87,61 +92,7 @@ function App() {
         <h2>Save time & money with exclusive offers from top stores</h2>
                  <img className='section_fourth-image' src={fourthMobile} alt='first phone'></img>
                  
-
         </section>
-
-      
-
-        {/* <div className='section_first-back'>
-
-          <div className='section_first-gadget'>
-           <h3>Find your Gadget</h3>
-           
-            <img className='section_first-image' src={man}></img>
-            <div className='section_first-blurred'>
-            <button className='section_first-button'>Get started</button>
-            </div>
-             
-          </div>
-
-        </div> */}
-
-        {/* <div className='section_first-description'>
-              <h4>Create an account</h4>
-          
-                 <h2>Discover original products</h2>
-            <p>There are more than 950 categories updated daily based on 
-              trending websites reviews an users rating.</p>      
-                
-           </div>
-           <img className='section_first-image' src={firstMobile} alt='first phone'></img>
-                
-       
-           <section className='section_second'>
-        <div className='section_second-description'>
-                 <h2>Experience products in AR</h2>
-              <p>Have you tried Augmented Reality? Stop
-                       looking at boring galleries and start
-                          experiences each item
-                  </p>
-                  <div className='circle'> 
-                  <img className='section_second-image' src={secondMobile} alt='second phone'></img>
-        
-                  </div>
-                  
-        </div>
-      </section >
-
-      <section className='section_therd'>
-      <div className='section_therd-description'>
-              <h4>Original platform</h4>
-          
-                 <h2>Hottest deals around the web</h2>
-            <p>Find the perfect gift or everyday goods right at your fingertips</p>      
-            <img className='section_therd-image' src={therdMobile} alt='first phone'></img>
-              
-           </div>
-      </section>  */}
       </main>
 
       <footer></footer>
