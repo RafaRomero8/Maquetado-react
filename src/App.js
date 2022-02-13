56aa3bba00256f26baec14ccbac8f9c3a50c17e7
RafaRomero8 <RafaRomerox@gmail.com>
2022-02-12 22:34:49 -0600

    drop comments

--- src/App.js
+++ src/App.js
@@ -127,56 +127,6 @@ function App() {
           ></img>
         </section>
 
-        {/* <div className='section_first-back'>
-
-          <div className='section_first-gadget'>
-           <h3>Find your Gadget</h3>
-           
-            <img className='section_first-image' src={man}></img>
-            <div className='section_first-blurred'>
-            <button className='section_first-button'>Get started</button>
-            </div>
-             
-          </div>
-
-        </div> */}
-
-        {/* <div className='section_first-description'>
-              <h4>Create an account</h4>
-          
-                 <h2>Discover original products</h2>
-            <p>There are more than 950 categories updated daily based on 
-              trending websites reviews an users rating.</p>      
-                
-           </div>
-           <img className='section_first-image' src={firstMobile} alt='first phone'></img>
-                
-       
-           <section className='section_second'>
-        <div className='section_second-description'>
-                 <h2>Experience products in AR</h2>
-              <p>Have you tried Augmented Reality? Stop
-                       looking at boring galleries and start
-                          experiences each item
-                  </p>
-                  <div className='circle'> 
-                  <img className='section_second-image' src={secondMobile} alt='second phone'></img>
-        
-                  </div>
-                  
-        </div>
-      </section >
-
-      <section className='section_therd'>
-      <div className='section_therd-description'>
-              <h4>Original platform</h4>
-          
-                 <h2>Hottest deals around the web</h2>
-            <p>Find the perfect gift or everyday goods right at your fingertips</p>      
-            <img className='section_therd-image' src={therdMobile} alt='first phone'></img>
-              
-           </div>
-      </section>  */}
       </main>
 
       <footer className="footer">
